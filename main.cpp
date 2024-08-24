5848f807ff43d53856663f7bd4f1112714c0a7fc
Olympus-win <10081158+mtm25@users.noreply.github.com>
2024-08-24 13:52:46 -0700

    Added to Visual Code Studio on Windows

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
-int main() {
-  std::cout << Hello Coder ! << std::endl
-return 0;
-
+int main()
+{
+  std::cout << "Hello Coder ! " << std::endl;
+  std::cout << "Setting up in VSC" << std::endl;
+  return 0;
 }
