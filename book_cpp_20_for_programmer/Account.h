0d94686053b7a98089e3566ccb98970625846418
Olympus-win <10081158+mtm25@users.noreply.github.com>
2024-10-19 16:06:02 -0700

    Add constructor for Account class.  Use member initialization list

--- book_cpp_20_for_programmer/Account.h
+++ book_cpp_20_for_programmer/Account.h
@@ -7,9 +7,20 @@ namespace book_cpp_20_for_programmers
 	class Account
 	{
 	public:
+		//custom constructor
+		//Account(std::string_view name) {
+		//	name_ = name; //name_ is initialized with name, copy be value
+		//}
+
+		//custom constructor using member initialization list
+		//single parameter constructors must also use explicit keyword to prevent
+		//implicit calls used in type conversions. 
+		Account(std::string_view name) : name_{name}
+		{}
+
 		// member function that sets m_name in the object
 		void setName(std::string_view name) {
-			m_name = name;
+			name_ = name;
 		}
 
 		// member function that retrieves the account name from the object
@@ -18,11 +29,11 @@ namespace book_cpp_20_for_programmers
 		// to any data members of the object of the Account class.
 		const std::string& getName() const
 		{
-			return m_name;
+			return name_;
 		}
 
 	private:
-		std::string m_name;// string variable that will be initialized to "" when account is created, 
+		std::string name_;// string variable that will be initialized to "" when account is created, 
 		//std::string m_name{ "test" };
 		
 	};
--- book_cpp_20_for_programmer/test_Account.cpp
+++ book_cpp_20_for_programmer/test_Account.cpp
@@ -3,17 +3,26 @@
 
 namespace book_cpp_20_for_programmers {
 	//Create account obj with default constructor'
-	void test_create_account_default_constructor() {
-		Account obj; //this will invoke the compiler created default constructor
+	//void test_create_account_default_constructor() {
+	//	Account obj; //this will invoke the compiler created default constructor
+	//	std::cout << "Account Name: " << obj.getName() << std::endl;
+
+	//	Account obj2; //this will invoke the compiler created default constructor
+	//	std::cout << "Account Name: " << obj2.getName() << std::endl;
+	//}
+
+	void test_create_account_custom_constructor() {
+		Account obj{"Joe Black"}; //this will invoke the compiler created default constructor
 		std::cout << "Account Name: " << obj.getName() << std::endl;
 
-		Account obj2; //this will invoke the compiler created default constructor
+		Account obj2{ "Donald Duck" }; //this will invoke the compiler created default constructor
 		std::cout << "Account Name: " << obj2.getName() << std::endl;
 	}
 
 		
 	void test_account() {
-		test_create_account_default_constructor();
+		//test_create_account_default_constructor();
+		test_create_account_custom_constructor();
 	}
 
 
